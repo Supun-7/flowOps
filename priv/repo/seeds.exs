7b8e5fa124cd96c15e4b9f59a3cfafd743337eb9
Supun Buddhima Dharmaratne <It24101504@my.sliit.lk>
2026-05-31 23:45:38 +0530

    feat:seeds.exs was updated

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,17 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     Flowops.Repo.insert!(%Flowops.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias Flowops.Accounts
+alias Flowops.Accounts.User
+alias Flowops.Repo
+
+# Create admin user
+case Accounts.get_user_by_email("admin@flowops.com") do
+  nil ->
+    %User{}
+    |> User.email_changeset(%{email: "admin@flowops.com"})
+    |> User.password_changeset(%{password: "Admin@2024Secure!"})
+    |> Repo.insert!()
+
+    IO.puts("✅ Admin user created: admin@flowops.com")
+
+  _user ->
+    IO.puts("ℹ️  Admin user already exists, skipping.")
+end
